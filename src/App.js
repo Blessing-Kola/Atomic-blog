d41d9c4088da760fb4608a77624ea105e4514acc
Blessingkola <Blessingoloyedekola@gmail.com>
2025-08-20 15:28:09 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,241 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
+//Step on in using Context API is to call and store createContext in a variable. It is stored as a component. 
+const PostContext = createContext()
+
+function App() {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isFakeDark, setIsFakeDark] = useState(false);
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+        `${post.title} ${post.body}`
+          .toLowerCase()
+          .includes(searchQuery.toLowerCase())
+      )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
+  useEffect(
+    function () {
+      document.documentElement.classList.toggle("fake-dark-mode");
+    },
+    [isFakeDark]
+  );
+
+  return (
+    //2. pass value to child component. 
+    //3. Consume the value in appropriate component.(for this tutorial where the values are cut off, I commented the values out.)
+
+
+    <PostContext.Provider value={{
+      posts: searchedPosts,
+      onAddPost: handleAddPost,
+      onClearPosts: handleClearPosts,
+      searchQuery,
+      setSearchQuery,
+    }}>
+      <section>
+        <button
+          onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+          className="btn-fake-dark-mode"
+        >
+          {isFakeDark ? "☀️" : "🌙"}
+        </button>
+
+        <Header
+        // posts={searchedPosts}
+        // onClearPosts={handleClearPosts}
+        // searchQuery={searchQuery}
+        // setSearchQuery={setSearchQuery}
+        />
+        {/* <Main posts={searchedPosts} onAddPost={handleAddPost} />
+        <Archive onAddPost={handleAddPost} />
+        <Footer /> */}
+        <Main posts={searchedPosts} onAddPost={handleAddPost} />
+        <Archive onAddPost={handleAddPost} />
+        <Footer />
+      </section>
+    </PostContext.Provider >
+  );
+}
+
+// the values below are removed from the function props.
+//{ posts, onClearPosts, searchQuery, setSearchQuery }
+
+function Header() {
+  //reading the removed props from context, we use useContext
+  const { onClearPosts } = useContext(PostContext)
+
+  return (
+    <header>
+      <h1>
+        <span>⚛️</span>The Atomic Blog
+      </h1>
+      {/* // posts={posts} */}
+      <div>
+        <Results />
+        <SearchPosts
+        // searchQuery={searchQuery}
+        // setSearchQuery={setSearchQuery}
+        />
+        <button onClick={onClearPosts}>Clear posts</button>
+      </div>
+    </header>
+  );
+}
+
+function SearchPosts() {
+
+  const { searchQuery, setSearchQuery } = useContext(PostContext)
+
+
+  return (
+    <input
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      placeholder="Search posts..."
+    />
+  );
+}
+
+// function Results({ posts }) {
+function Results() {
+
+  const { posts } = useContext(PostContext)
+
+  return <p>🚀 {posts.length} atomic posts found</p>;
+}
+
+// function Main({ posts, onAddPost }) {
+function Main() {
+  return (
+    <main>
+      {/* <FormAddPost onAddPost={onAddPost} />
+      <Posts posts={posts} /> */}
+      <FormAddPost />
+      <Posts />
+    </main>
+  );
+}
+
+// function Posts({ posts }) {
+function Posts() {
+  return (
+    <section>
+      <List />
+    </section>
+  );
+}
+
+// function FormAddPost({ onAddPost }) {
+function FormAddPost() {
+
+  const { onAddPost } = useContext(PostContext)
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+    if (!body || !title) return;
+    onAddPost({ title, body });
+    setTitle("");
+    setBody("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Post title"
+      />
+      <textarea
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+        placeholder="Post body"
+      />
+      <button>Add post</button>
+    </form>
+  );
+}
+
+// function List({ posts }) {
+function List() {
+
+  const { posts } = useContext(PostContext)
+
+  return (
+    <ul>
+      {posts.map((post, i) => (
+        <li key={i}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+// function Archive({ onAddPost }) {
+function Archive() {
+
+  const { onAddPost } = useContext(PostContext)
+
+  // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
+  const [posts] = useState(() =>
+    // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
+    Array.from({ length: 10000 }, () => createRandomPost())
+  );
+
+  const [showArchive, setShowArchive] = useState(false);
+
+  return (
+    <aside>
+      <h2>Post archive</h2>
+      <button onClick={() => setShowArchive((s) => !s)}>
+        {showArchive ? "Hide archive posts" : "Show archive posts"}
+      </button>
+
+      {showArchive && (
+        <ul>
+          {posts.map((post, i) => (
+            <li key={i}>
+              <p>
+                <strong>{post.title}:</strong> {post.body}
+              </p>
+              <button onClick={() => onAddPost(post)}>Add as new post</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </aside>
+  );
+}
+
+function Footer() {
+  return <footer> &copy; by The Atomic Blog ✌️</footer>;
+}
+
+export default App;
