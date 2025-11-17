d7a73c769abb1959a69c978f2cbb28660681eb2e
Kola Blessing <blessingoloyedekola@gmail.com>
2025-11-17 06:43:33 +0100

    First publish

--- src/App-v1.js
+++ src/App-v1.js
@@ -8,31 +8,29 @@ function createRandomPost() {
   };
 }
 
-
 //Step on in using Context API is to call and store createContext in a variable. It is stored as a component. 
-// const PostContext = createContext()
+const PostContext = createContext()
 
 function App() {
-  // const [posts, setPosts] = useState(() =>
-  //   Array.from({ length: 30 }, () => createRandomPost())
-  // );
-  // const [searchQuery, setSearchQuery] = useState("");
-  // const [isFakeDark, setIsFakeDark] = useState(false);
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isFakeDark, setIsFakeDark] = useState(false);
 
-  // // Derived state. These are the posts that will actually be displayed
-  // const searchedPosts =
-  //   searchQuery.length > 0
-  //     ? posts.filter((post) =>
-  //       `${post.title} ${post.body}`
-  //         .toLowerCase()
-  //         .includes(searchQuery.toLowerCase())
-  //     )
-  //     : posts;
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+        `${post.title} ${post.body}`
+          .toLowerCase()
+          .includes(searchQuery.toLowerCase())
+      )
+      : posts;
 
-  // function handleAddPost(post) {
-  //   setPosts((posts) => [post, ...posts]);
-  // }
-  // 
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
 
   function handleClearPosts() {
     setPosts([]);
